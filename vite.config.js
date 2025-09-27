a7eac79aac5c7e631508d4c9aca8be028fba7719
Adhithya200503 <168291465+Adhithya200503@users.noreply.github.com>
2025-09-27 15:02:38 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: true,
-    port: 5173,
-    allowedHosts: [
-      "7c9a44aa4cd0.ngrok-free.app"
-    ],
-  },
 })
